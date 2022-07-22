8c6056c0808eaa28eebf213e698c48ad8c7550da
Abbas Cheshmapoosh <cheshmapoosh@gmail.com>
2022-07-22 08:31:47 -0700

    resolve dont send origin by vpn

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,13 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
+  {
+    name: 'strapi::poweredBy',
+    config: {
+      poweredBy: "Chamrosh Co"
+    }
+
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
